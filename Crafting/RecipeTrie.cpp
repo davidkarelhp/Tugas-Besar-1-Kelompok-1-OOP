5004c6dd4ab1ae92b20261677487035f9a9470ab
davidkarelhp <13520154@std.stei.itb.ac.id>
2022-03-25 17:28:17 +0700

    Update Makefile

--- Crafting/RecipeTrie.cpp
+++ Crafting/RecipeTrie.cpp
@@ -43,7 +43,6 @@ void RecipeTrie::buildRecipe(int row, int col, string* recipeArr, string result,
     currentNode = this->root;
     int currentIdx = 0;
     for (i = 0; i < row; i++) {
-        // cout << i << " ";
         for (int j = 0; j < 3; j++) {
             if (j < col) {
                 if (!(recipeArr[currentIdx] == "-" && !found)) {
@@ -51,7 +50,6 @@ void RecipeTrie::buildRecipe(int row, int col, string* recipeArr, string result,
                         currentNode->set(recipeArr[currentIdx], new TrieNode());
                     }
                     currentNode = currentNode->get(recipeArr[currentIdx]);
-                    // cout << recipeArr[currentIdx] << " ";
                 }
 
                 if (!found && recipeArr[currentIdx] != "-") {
@@ -62,12 +60,10 @@ void RecipeTrie::buildRecipe(int row, int col, string* recipeArr, string result,
                 if (currentNode->get("-") == nullptr) {
                     currentNode->set("-", new TrieNode());
                 }
-                // cout << "- ";
                 currentNode = currentNode->get("-");
             } 
         }
     }
-    // cout << "\n";
     currentNode->set("*", (TrieNode*)resultTriplet);
 }
 
--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ EXT_OUT = out
 EXT_ANS = ans
 EXECUTABLE_FILENAME = main
 ALL_SRCS := $(wildcard Exception/*.hpp) $(wildcard IO/outputFile/*.cpp) $(wildcard IO/inputItem/*.cpp) $(wildcard IO/inputRecipe/*.cpp) $(wildcard Item/*.cpp) $(wildcard Inventory/*.cpp) $(wildcard Crafting/*.cpp) $(wildcard Player/*.cpp) $(wildcard *.cpp)
-SRCS     := $(filter-out check.cpp, $(ALL_SRCS))
+SRCS     := $(filter-out check.cpp Item/driver_item.cpp, $(ALL_SRCS))
 
 all: compile test check
 
